5d98e71b80c8b698fd85cd1bedd0352c22d04aa0
Scott Nowicki <scottzilla@users.noreply.github.com>
2017-11-09 08:18:41 -0600

    zsh, oh-my-zsh, and marked over macdown

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install zsh zsh-completions
 brew install tree
 
 # install cask
@@ -12,12 +13,18 @@ brew cask install vagrant-manager
 brew cask install https://raw.githubusercontent.com/caskroom/homebrew-cask/e8816187ae43f52b598f15f45b3453e22727ac99/Casks/virtualbox.rb
 brew cask install docker
 brew cask install chefdk
-brew cask install macdown
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #install apps from app store:
 
 #brew install mas
 #brew search app magnet
 #brew search app xcode
+#brew search app marked/marked2
 #brew install application <magnet#>
 #brew install application <xcode#>
+#brew install application <marked2#>
+
+# TODO: install iterm2
